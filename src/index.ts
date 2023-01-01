21f8ea5bc67cbd7209d1d16abb55bbf2ff32c92f
Chris Flannery <chris.flannery@koneksahealth.com>
2023-01-01 17:37:48 -0500

    connect apollo middleware

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,38 @@ import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
 import { Post } from './entities/Post';
 import mikroOrmConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
   const emFork = orm.em.fork();
 
-  // const post = emFork.create(Post, { title: 'my first post' });
-  // await emFork.persistAndFlush(post);
-  // const posts = await emFork.find(Post, {});
-  // console.log(posts);
+  const app = express();
+  let apolloServer = null;
+
+  async function startServer() {
+    apolloServer = new ApolloServer({
+      schema: await buildSchema({
+        resolvers: [HelloResolver],
+        validate: false,
+      }),
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+  }
+  startServer();
+
+  app.get('/', (_, res) => {
+    res.json({ data: 'api working' })
+  })
+
+  app.listen(4000, () => {
+    console.log('server started on localhost:4000');
+  });
 }
 
 
